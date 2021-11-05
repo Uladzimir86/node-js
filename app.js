b668474e34c0ca651fea715074dda09ba3ef47a2
Владимир Сахончик <sakhonchykui@asup.local>
2021-11-05 17:54:13 +0300

    add reading and writing data from file

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-// import { rot } from "./index.js";
 const func = require("./index.js");
-let {rot, caesar, atbash} = func;
-const str = 'abc ABC ччч z Z';
+const fs = require('fs');
+
+const {rot, caesar, atbash} = func;
+let readableStream;
+let writeableStream;
+
 const arr = process.argv.slice(2);
 
 if (arr[0] === '-c' || arr[0] === '--config') {
+
+    if (arr[2] === '-i') readableStream = fs.createReadStream(arr[3], "utf8");
+    if (arr[2] === '-o') writeableStream = fs.createWriteStream(arr[3]);
+    if (arr[4] === '-o') writeableStream = fs.createWriteStream(arr[5]);
+    
     const ciphers = arr[1]?.split('-');
-    console.log(ciphers )
+    const coding = (text = '') => {
+           let str = text;
 
     if (ciphers) {
         console.log('item')
@@ -15,26 +24,44 @@ if (arr[0] === '-c' || arr[0] === '--config') {
             switch (item) {
                 case 'C0':
                     console.log('c0',caesar(false, str))
+                    str = caesar(false, str);
                     break;
                 case 'C1':
                     console.log('c1',caesar(true, str))
+                    str = caesar(true, str);
                     break;
                 case 'R0':
                     console.log('r0',rot(false, str))
+                    str = rot(false, str);
                     break;
                 case 'R1':
-                    console.log('r1',rot(true), str);
+                    console.log('r1',rot(true, str));
+                    str = rot(true, str);
                     break;
                 case 'A':
                     console.log('a',atbash(str))
+                    str = atbash(str);
                     break;
                 default: throw new Error('hernja napisana1');
 
             }
         })
     } else throw new Error('hernja napisana3')
-
-
+    return str;
+    }
  
+// readableStream?.pipe(coding).pipe(writeableStream);
+readableStream?.on("data", function(chunk){
+    console.log('chunk',chunk)
+    writeableStream?.end(coding(chunk));
+});
+
 
 } else throw new Error('hernja napisana2')
+
+
+
+
+
+
+
